ec7ab83a0792a06cf0163fcdcba02a62cf61227a
Chris Marshall <chris@chrismar035.com>
2014-07-18 18:42:14 -0400

    Improve betting public API

--- betting.go
+++ betting.go
@@ -2,18 +2,27 @@ package craps
 
 import "math"
 
-type Bet struct {
+type PlayerBet struct {
 	amount int
-	odds Odds
+	bet Bet
 }
 
-type Odds struct {
+type Bet struct {
 	toPay int
 	base int
 }
 
-func (b Bet) Winnings() int {
-	odds := float64(b.odds.toPay) / float64(b.odds.base)
+func (b PlayerBet) Winnings() int {
+	odds := float64(b.bet.toPay) / float64(b.bet.base)
 	return int(math.Floor(float64(b.amount) * odds))
 }
 
+func (b PlayerBet) toPay() int { return b.bet.toPay }
+func (b PlayerBet) base() int { return b.bet.base }
+
+
+var passLineOdds =			Bet{1, 1}
+var fourTenPassLineOdds =	Bet{2, 1}
+var fourTenPlaceOdds =		Bet{9, 5}
+var fiveNineOdds =			Bet{3 ,2}
+var sixEightOdds =			Bet{6, 5}
--- betting_test.go
+++ betting_test.go
@@ -3,18 +3,20 @@ package craps
 import "testing"
 
 var payoutTests = []struct {
-	bet Bet
+	bet PlayerBet
 	out int
 }{
-	{Bet{5, Odds{7, 5}}, 7},
-	{Bet{10, Odds{7, 5}}, 14},
+	{PlayerBet{5, Bet{7, 5}}, 7},
+	{PlayerBet{10, Bet{7, 5}}, 14},
+	{PlayerBet{10, passLineOdds}, 10},
+	{PlayerBet{10, fourTenPassLineOdds}, 20},
 }
 
 func TestWinnings(t *testing.T) {
 	for _, tt := range payoutTests {
 		if x := tt.bet.Winnings(); x != tt.out {
 			t.Errorf("Bet{%v at %v:%v).Winnings() = %v, want %v",
-				tt.bet.amount, tt.bet.odds.toPay, tt.bet.odds.base, x, tt.out)
+				tt.bet.amount, tt.bet.toPay, tt.bet.base, x, tt.out)
 		}
 	}
 }
